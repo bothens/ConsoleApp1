54178f09e2838539a1037eb0b0267c5b088a9b91
Alexander Bothen <alexanderbothen@icloud.com>
2025-03-18 22:42:14 +0100

    ändring

--- Program.cs
+++ Program.cs
@@ -1,117 +1,167 @@
-﻿using Spectre.Console;
+﻿using System;
+using System.Collections.Generic;
+using Spectre.Console;
 
-namespace ConsoleApp1
-{
-    class Program
+class Program
 {
     static void Main()
     {
-        // Startmeddelande
+        Console.WriteLine("Spelet startar..."); // Debug-meddelande
+        HangmanGame spel = new HangmanGame();
+        spel.Starta();
+    }
+}
+
+// Observer Interface
+interface IObserver
+{
+    void Uppdatera(string meddelande);
+}
+
+// Hangman-spelet med Observer Pattern
+class HangmanGame
+{
+    private string hemligtOrd;
+    private HashSet<char> gissadeBokstäver = new HashSet<char>();
+    private int felaktigaGissningar = 0;
+    private const int maxFel = 6;
+    private List<IObserver> observatörer = new List<IObserver>();
+
+    public HangmanGame()
+    {
+        // Skapa en lista med ord och blanda dem
+        string[] ordlista = {
+            "programmering", "spel", "dator", "visualstudio", "console",
+            "banan", "skola", "lärare", "elefant", "giraff",
+            "konsert", "guitar", "solsken", "måne", "stjärna"
+        };
+        FisherYatesShuffle(ordlista);
+        hemligtOrd = ordlista[0];
+
+        // Lägg till observatörer
+        LäggTillObservatör(new Gallgen());
+        LäggTillObservatör(new Ordpanel());
+    }
+
+    public void Starta()
+    {
         AnsiConsole.MarkupLine("[bold yellow]Välkommen till Hangman![/]");
-        AnsiConsole.MarkupLine("[italic grey]Gissa ordet innan hängningen är komplett![/]");
-
-        // Spelets variabler
-        string[] ordlista = { "programmering", "spel", "dator", "visualstudio", "console" };
-        var slump = new Random();
-        string hemligtOrd = ordlista[slump.Next(ordlista.Length)];
-        HashSet<char> gissadeBokstäver = new HashSet<char>();
-        int felaktigaGissningar = 0;
-        int maxFel = 6;
-
-        // Spel-loop
-        while (true)
+        while (felaktigaGissningar < maxFel)
         {
-            // Visa gallgen
-            VisaGallgen(felaktigaGissningar);
+            VisaStatus();
+            char gissning = LäsGiltigBokstav();
 
-            // Visa ordet
-            AnsiConsole.MarkupLine("[bold cyan]Ord:[/] " + VisaOrd(hemligtOrd, gissadeBokstäver));
-
-            // Kontrollera om spelaren har vunnit
-            if (AllaBokstäverGissade(hemligtOrd, gissadeBokstäver))
-            {
-                AnsiConsole.MarkupLine("[bold green]Grattis! Du vann![/]");
-                break;
-            }
-
-            // Kontrollera om spelaren har förlorat
-            if (felaktigaGissningar >= maxFel)
-            {
-                AnsiConsole.MarkupLine($"[bold red]Du förlorade! Ordet var: {hemligtOrd}[/]");
-                break;
-            }
-
-            // Fråga användaren om en gissning
-            char gissning = AnsiConsole.Ask<char>("[bold yellow]Gissa en bokstav:[/] ");
-
-            // Kontrollera om bokstaven redan är gissad
             if (gissadeBokstäver.Contains(gissning))
             {
                 AnsiConsole.MarkupLine("[bold red]Du har redan gissat den bokstaven![/]");
                 continue;
             }
 
-            // Lägg till gissningen till mängden gissade bokstäver
             gissadeBokstäver.Add(gissning);
 
-            // Kontrollera om gissningen är korrekt
-            if (!hemligtOrd.Contains(gissning))
+            if (hemligtOrd.Contains(gissning))
             {
-                felaktigaGissningar++;
-                AnsiConsole.MarkupLine("[bold red]Fel gissning![/]");
+                SkickaNotis("[bold green]Rätt gissning![/]");
+                if (AllaBokstäverGissade())
+                {
+                    AnsiConsole.MarkupLine($"[bold green]Grattis! Du vann! Ordet var: {hemligtOrd}[/]");
+                    return;
+                }
             }
             else
             {
-                AnsiConsole.MarkupLine("[bold green]Rätt gissning![/]");
+                felaktigaGissningar++;
+                SkickaNotis("[bold red]Fel gissning![/]");
             }
         }
+
+        AnsiConsole.MarkupLine($"[bold red]Du förlorade! Ordet var: {hemligtOrd}[/]");
     }
 
-    static void VisaGallgen(int felaktigaGissningar)
+    private char LäsGiltigBokstav()
     {
-        string[] gallge = {
-            "  +---+  ",
-            "  |   |  ",
-            "      |  ",
-            "      |  ",
-            "      |  ",
-            "      |  ",
-            "========="
-        };
-
-        if (felaktigaGissningar > 0) gallge[2] = "  O   |  ";
-        if (felaktigaGissningar > 1) gallge[3] = "  |   |  ";
-        if (felaktigaGissningar > 2) gallge[3] = " /|   |  ";
-        if (felaktigaGissningar > 3) gallge[3] = " /|\\  |  ";
-        if (felaktigaGissningar > 4) gallge[4] = " /    |  ";
-        if (felaktigaGissningar > 5) gallge[4] = " / \\  |  ";
-
-        foreach (string rad in gallge)
+        while (true)
         {
-            AnsiConsole.MarkupLine($"[grey]{rad}[/]");
+            string input = AnsiConsole.Ask<string>("[bold yellow]Gissa en bokstav:[/] ").Trim().ToLower();
+            if (input.Length == 1 && char.IsLetter(input[0]))
+                return input[0];
+
+            AnsiConsole.MarkupLine("[bold red]Felaktig inmatning! Ange EN bokstav.[/]");
         }
     }
 
-    static string VisaOrd(string hemligtOrd, HashSet<char> gissadeBokstäver)
+    private void VisaStatus()
     {
-        var visatOrd = "";
+        AnsiConsole.MarkupLine($"\n[bold cyan]Felaktiga gissningar:[/] {felaktigaGissningar}/{maxFel}");
+        AnsiConsole.Markup("[bold cyan]Ord:[/] ");
         foreach (char bokstav in hemligtOrd)
-        {
-            visatOrd += gissadeBokstäver.Contains(bokstav) ? $"{bokstav} " : "_ ";
-        }
-        return visatOrd.Trim();
+            AnsiConsole.Write(gissadeBokstäver.Contains(bokstav) ? $"{bokstav} " : "_ ");
+        Console.WriteLine();
+
+        // Visa stickgubben baserat på antalet felaktiga gissningar
+        RitaStickgubbe(felaktigaGissningar);
     }
 
-    static bool AllaBokstäverGissade(string hemligtOrd, HashSet<char> gissadeBokstäver)
+    private bool AllaBokstäverGissade()
     {
         foreach (char bokstav in hemligtOrd)
-        {
             if (!gissadeBokstäver.Contains(bokstav))
-            {
                 return false;
-            }
-        }
         return true;
     }
+
+    private void LäggTillObservatör(IObserver observatör)
+    {
+        observatörer.Add(observatör);
+    }
+
+    private void SkickaNotis(string meddelande)
+    {
+        foreach (var obs in observatörer)
+            obs.Uppdatera(meddelande);
+    }
+
+    private void FisherYatesShuffle(string[] lista)
+    {
+        Random rand = new Random();
+        for (int i = lista.Length - 1; i > 0; i--)
+        {
+            int j = rand.Next(i + 1);
+            (lista[i], lista[j]) = (lista[j], lista[i]);
+        }
+    }
+
+    // Metod för att rita stickgubben
+    private void RitaStickgubbe(int felaktigaGissningar)
+    {
+        string[] stickgubbe = {
+            "  ____ \n |    |\n |    \n |    \n |    \n_|_",
+            "  ____ \n |    |\n |    O\n |    \n |    \n_|_",
+            "  ____ \n |    |\n |    O\n |    |\n |    \n_|_",
+            "  ____ \n |    |\n |    O\n |   /|\n |    \n_|_",
+            "  ____ \n |    |\n |    O\n |   /|\\\n |    \n_|_",
+            "  ____ \n |    |\n |    O\n |   /|\\\n |   / \n_|_",
+            "  ____ \n |    |\n |    O\n |   /|\\\n |   / \\\n_|_"
+        };
+
+        AnsiConsole.MarkupLine($"[bold red]{stickgubbe[felaktigaGissningar]}[/]");
+    }
 }
+
+// Gallgen-observatör
+class Gallgen : IObserver
+{
+    public void Uppdatera(string meddelande)
+    {
+        AnsiConsole.MarkupLine($"[italic grey](Gallgen uppdaterad: {meddelande})[/]");
+    }
+}
+
+// Ordpanel-observatör
+class Ordpanel : IObserver
+{
+    public void Uppdatera(string meddelande)
+    {
+        AnsiConsole.MarkupLine($"[italic grey](Ordpanel uppdaterad: {meddelande})[/]");
+    }
 }
